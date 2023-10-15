d161a302f191fe6fc51d3796e412553c8b461b84
syny-pulse <sinaijose002@gmail.com>
2023-10-16 01:10:56 +0300

    day 0ne

---
+++ pr1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    int n, sum = 0;
+    printf("Enter a positive integer: \n");
+    scanf("%d", &n);
+    for(int i=0;i<n;i++) {
+        if(i%5==0|i%3==0) {
+            sum = sum + i;
+            i++;
+        }
+    printf("%d",sum);
+    }
+    return 0;
+}
